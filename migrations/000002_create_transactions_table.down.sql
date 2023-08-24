7ea0da18436abe1ef9157f9f37ffdd6dadb9e429
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-08-25 00:41:34 +0700

    Create transactions migration table

---
+++ migrations/000002_create_transactions_table.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE transactions;
+
+DROP TYPE transactiontype;
+
+DROP TYPE transactionstatus;
---
+++ migrations/000002_create_transactions_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TYPE transactionstatus AS ENUM ('success', 'failed');
+
+CREATE TYPE transactiontype AS ENUM ('deposit', 'withdrawal');
+
+CREATE TABLE transactions
+(
+    id            uuid NOT NULL,
+    wallet_id     uuid NOT NULL,
+    status        transactionstatus NOT NULL,
+    transacted_at timestamp NOT NULL,
+    type          transactiontype NOT NULL,
+    amount        real NOT NULL,
+    reference_id  uuid NOT NULL,
+    CONSTRAINT transactions_pk PRIMARY KEY (id),
+    CONSTRAINT transactions_wallets_fk FOREIGN KEY (wallet_id) REFERENCES wallets (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
