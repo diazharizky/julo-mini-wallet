e2d57b3b2459937a72c2d1f4dfef15c574358ca2
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-08-25 09:44:49 +0700

    Enable locking on disable wallet

--- internal/modules/disable_wallet.go
+++ internal/modules/disable_wallet.go
@@ -4,6 +4,7 @@ import (
 	"github.com/diazharizky/julo-mini-wallet/internal/app"
 	"github.com/diazharizky/julo-mini-wallet/internal/models"
 	"github.com/google/uuid"
+	"gorm.io/gorm/clause"
 )
 
 type disableWalletModule struct {
@@ -15,7 +16,7 @@ func NewDisableWalletModule(appCtx app.Ctx) disableWalletModule {
 }
 
 func (m disableWalletModule) Call(accountID uuid.UUID) (*models.Wallet, error) {
-	wallet, err := m.appCtx.WalletRepository.GetByAccountID(accountID)
+	wallet, err := m.appCtx.WalletRepository.GetByAccountID(accountID, clause.Locking{Strength: "UPDATE"})
 	if err != nil {
 		return nil, err
 	}
